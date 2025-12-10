a8710dccd5021e31445dd64d1223ed43bc58dc91
kkjw1 <kkjjoo1212@gmail.com>
2025-12-10 13:43:26 +0900

    주문 도메인

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.Exception.NotEnoughStockException;
 import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,5 +26,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 엔티티 자체에서 해결할 수 있는 것은 엔티티 자체에서 처리하는 것이 좋음
+    // service 계층에서 setter로 넣고 뺴는것은 좋지않음
+
+    /**
+     * 제고 증가 (stock)
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 제고 감소 (stock)
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
